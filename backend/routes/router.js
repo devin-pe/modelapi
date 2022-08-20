6c6ee5a964f4b7f031c8bd7e15d183e2a3e2b662
Devin Pereira <devpereira1@gmail.com>
2022-08-20 16:48:19 -0400

    REST API modified

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const {getModels, setModel, delModel, createHomepage} = require('../controllers/methods')
 
 router.route('/').get(createHomepage)
-router.route('/get').get(getModels)
+router.route('/get').get(getModels) //change this (GET ALL) and post to same route later
 router.route('/uploads').post(setModel)
+router.route('/:id').delete(delModel)
 module.exports = {router}
